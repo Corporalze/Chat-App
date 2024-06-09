f2315e1becf20d596da9964c876bfd8d05b23be0
Corporalze <taha.sadouki@gmail.com>
2024-06-09 09:57:54 +0100

    img upload

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,30 +1,48 @@
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "./firebase";
+import crypto from 'crypto';
+
+const hashFile = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const buffer = e.target.result;
+      const hash = crypto.createHash('sha256');
+      hash.update(new Uint8Array(buffer));
+      resolve(hash.digest('hex'));
+    };
+    reader.onerror = (err) => {
+      reject(err);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+};
 
 const upload = async (file) => {
-
-    const date = new Date()
-
-    const storageRef = ref(storage, `images/${date + file.name}`);
-
+  try {
+    const hash = await hashFile(file);
+    const storageRef = ref(storage, `images/${hash + file.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    return new Promise((resolve, reject)=>{
-
-        uploadTask.on('state_changed', 
+    return new Promise((resolve, reject) => {
+      uploadTask.on('state_changed',
         (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-        }, 
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+        },
         (error) => {
-            reject("Something went wrong!" + error.code)
-        }, 
+          reject("Something went wrong!" + error.code);
+        },
         () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL)
-            });
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
         }
-    )}
-)};
+      );
+    });
+  } catch (error) {
+    console.error('Error generating file hash:', error);
+  }
+};
 
 export default upload;
